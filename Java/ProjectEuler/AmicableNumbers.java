80bb3e7bf214c69122030425769f9fc96351754d
Noah Prime <primen@wwu.edu>
2019-03-11 15:06:37 -0700

    Code for finding the sum of the AmicableNumbers under 10,000

--- Java/ProjectEuler/AmicableNumbers.java
+++ Java/ProjectEuler/AmicableNumbers.java
@@ -1,23 +1,38 @@
+import java.math.BigInteger;
+
 public class AmicableNumbers {
     public static void main(String[] args){
-        int[][] arr = new int[1000][2];
-        fillArr(arr);
-
+      int[][] arr = new int[10000][2];
+      fillArr(arr);
+      System.out.println("The sum of amicable numbers less than 10,000 = " + sumAmicable(arr).toString());
     }
 
     public static void fillArr(int[][] arr){
-        for(int i = 1;i < 1001;i++){
-            arr[i-1][0] = i;
-            arr[i-1][1] = findSumDivisors(i);
-        }
+      for(int i = 1;i < 10000;i++){
+        arr[i-1][0] = i;
+        arr[i-1][1] = findSumDivisors(i);
+      }
     }
 
     public static int findSumDivisors(int n){
-
-        return 0;
+      int sum = 0;
+      for(int i = 1;i < n;i++){
+        if(n % i == 0){
+          sum += i;
+        }
+      }
+      return sum;
     }
 
-    public static void sumAmicable(){
-
+    public static BigInteger sumAmicable(int[][] arr){
+      BigInteger sum = BigInteger.valueOf(0);
+      for(int i = 2;i < 10000;i++){
+        if(arr[i-1][1] < 10000){
+          if(i == arr[(arr[i-1][1])-1][1] && i != arr[i-1][1]){
+            sum = sum.add(BigInteger.valueOf(i));
+          }
+        }
+      }
+      return sum;
     }
 }
