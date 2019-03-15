ba6df3a2baccf69a49bea430da215ec9ee2c98c7
Noah Prime <primen@wwu.edu>
2019-03-15 10:10:12 -0700

    Working code to find sum  of 'non-abundant' numbers

--- Java/ProjectEuler/NonAbundantSums.java
+++ Java/ProjectEuler/NonAbundantSums.java
@@ -1,13 +1,17 @@
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class NonAbundantSums {
-  static HashMap abundantNums;
+  static HashMap<BigInteger,BigInteger> unwanted;
+  static ArrayList<BigInteger> abundantNums;
 
   public static void main(String[] args){
-    abundantNums = new HashMap();
-    fillMap(abundantNums);
-    System.out.println("# of abundant numbers less than 28,123 = " + abundantNums.size());
+    unwanted = new HashMap<BigInteger,BigInteger>();
+    abundantNums = new ArrayList<BigInteger>();
+    fillArr();
+    findUnwanted();
+    System.out.println("Sum of non-abundant numbers = " + sumWanted().toString());
   }
 
   public static int findSumDivisors(int n){
@@ -20,12 +24,33 @@ public class NonAbundantSums {
     return sum;
   }
 
-  public static void fillMap(HashMap map){
+  public static void fillArr(){
     for(int i = 1;i < 28123;i++){
       if(findSumDivisors(i) > i) {
-        abundantNums.put(BigInteger.valueOf(i),BigInteger.valueOf(i));
+        abundantNums.add(BigInteger.valueOf(i));
       }
     }
+  }
+
+  public static void findUnwanted() {
+    for(int i = 0;i < abundantNums.size();i++) {
+      for(int j = 0;j < abundantNums.size();j++) {
+        BigInteger sum = abundantNums.get(i).add(abundantNums.get(j));
+        if(!unwanted.containsKey(sum)) {
+          unwanted.put(sum,sum);
+        }
+      }
+    }
+  }
+
+  public static BigInteger sumWanted() {
+    BigInteger sum = BigInteger.valueOf(0);
+    for(int i = 1; i < 28123;i++) {
+      if(!unwanted.containsKey(BigInteger.valueOf(i))) {
+        sum = sum.add(BigInteger.valueOf(i));
+      }
+    }
+    return sum;
   }
 
 }
